66b9deb5ffad1d8a2c999af1a86134d1ed8ac813
Kantan9 <93031996+Kantan9@users.noreply.github.com>
2022-04-19 09:22:58 +0530

    Add files via upload

---
+++ MicroWave.js
@@ -0,0 +1,43 @@
+img="";
+objects=[];
+
+status="";
+
+function preload()
+{
+    img=loadImage("McroWave.jpg");
+}
+
+function setup()
+{
+    canvas=createCanvas(620,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("results").innerHTML="status:detecting objects";
+}
+
+function modelLoaded()
+{
+    console.log("modelLoded");
+    status=true;
+    objectDetector.detect(img,gotresults);
+}
+
+function gotresults(error,results)
+{
+  if(error)
+  {
+      console.log(error);
+  }
+  console.log(results);
+  objects=results;
+}
+
+function main()
+{
+    window.location="index.html"
+}
+
+function draw(){
+    image(img,0,0,620,420);
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function page1()
+{
+    window.location="Bottle.html"
+}
+
+function page2()
+{
+    window.location="Clock.html"
+}
+
+function page3()
+{
+    window.location="Fan.html"
+}
+
+function page4()
+{
+    window.location="MicroWave.html"
+}
